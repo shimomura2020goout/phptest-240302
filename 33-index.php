5359491e2b335198064735c4f4f21821cb610dfe
shimomurahiroki <69880795+shimomura2020goout@users.noreply.github.com>
2024-03-11 22:04:03 +0900

    20240311Study

---
+++ 33-index.php
@@ -0,0 +1,11 @@
+<?php
+
+$test = 123;
+$test_2 = 456;
+
+echo $test;
+var_dump($test);
+exit;
+echo $test_2;
+
+?>
---
+++ form/input.php
@@ -0,0 +1,60 @@
+<?php
+
+if(!empty($_POST)){
+  echo '<pre>';
+  var_dump($_POST);
+  echo '</pre>';
+}
+
+$pageFlag = 0;
+
+if(!empty($_POST['btn_confirm'])){
+  $pageFlag = 1;
+}
+if(!empty($_POST['btn_submit'])){
+  $pageFlag = 2;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+
+<?php if($pageFlag === 1 ) : ?>
+  <form method="POST" action="input.php">
+    氏名
+  <?php echo $_POST['your_name'];?>
+    <br>
+  メールアドレス
+  <?php echo $_POST['email'];?>
+    <br>
+  <input type="submit" name="back" value="戻る">
+  <input type="submit" name="btn_submit" value="送信する">
+  <input type="hidden" name="your_name" value="<?php echo $_POST['your_name'];?>">
+  <input type="hidden" name="email" value="<?php echo $_POST['email'];?>">
+</form>
+<?php endif; ?>
+
+<?php if($pageFlag === 2 ) : ?>
+送信完了しました。
+<?php endif; ?>
+
+<?php if($pageFlag === 0 ) : ?>
+  <form method="POST" action="input.php">
+    氏名
+  <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo $_POST['your_name'];} ?>">
+  <br>
+  メールアドレス
+  <input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo $_POST['email'];} ?>">
+  <br>
+  <input type="submit" name="btn_confirm" value="確認する">
+  </form>
+<?php endif; ?>
+</body>
+</html>
