3b338fee10ed3d6510deddf1853aaa7e24592a1a
shimomurahiroki <69880795+shimomura2020goout@users.noreply.github.com>
2024-03-16 21:34:25 +0900

    38までinputphpで対応済み

--- 38-index.php
+++ 38-index.php
@@ -1,11 +1,10 @@
 <?php
-session_start();
-header('X-FRAME-OPTIONS:DENY');
-if(!empty($_SESSION)){
-  echo '<pre>';
-  var_dump($_SESSION);
-  echo '</pre>';
-}
+
+// if(!empty($_POST)){
+//   echo '<pre>';
+//   var_dump($_POST);
+//   echo '</pre>';
+// }
 
 function h($str) {
     return htmlspecialchars($str, ENT_QUOTES,"UTF-8");
@@ -32,9 +31,6 @@ if(!empty($_POST['btn_submit'])){
 
 
 <?php if($pageFlag === 1 ) : ?>
-<?php 
-  if($_POST['csrf'] === $_SESSION['csrfToken'])
-:?>
   <form method="POST" action="input.php">
     氏名
   <?php echo h($_POST['your_name']) ;?>
@@ -46,28 +42,14 @@ if(!empty($_POST['btn_submit'])){
   <input type="submit" name="btn_submit" value="送信する">
   <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']) ;?>">
   <input type="hidden" name="email" value="<?php echo h($_POST['email']) ;?>">
-  <input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']) ;?>">
 </form>
-<?php endif; ?>
 <?php endif; ?>
 
 <?php if($pageFlag === 2 ) : ?>
-  <?php 
-  if($_POST['csrf'] === $_SESSION['csrfToken'])
-:?>
-  送信完了しました。
-<?php unset($_SESSION['csrfToken']); ?>
-<?php endif; ?>
+送信完了しました。
 <?php endif; ?>
 
 <?php if($pageFlag === 0 ) : ?>
-<?php
-  if(!isset($_SESSION['csrfToken'])){
-    $csrfToken = bin2hex(random_bytes(32));
-    $_SESSION['csrfToken'] = $csrfToken;
-  }
-  $token = $_SESSION['csrfToken']; //短い変数で戻り値として活用したいために変数化した
-?>
   <form method="POST" action="input.php">
     氏名
   <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']) ;} ?>">
@@ -75,7 +57,6 @@ if(!empty($_POST['btn_submit'])){
   メールアドレス
   <input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']) ;} ?>">
   <br>
-  <input type="hidden" name="csrf" value="<?php echo $token; ?>">
   <input type="submit" name="btn_confirm" value="確認する">
   </form>
 <?php endif; ?>
